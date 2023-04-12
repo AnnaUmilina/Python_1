ff1f357db20c569022856d98e36474eb6059af58
Anna <umilina98@mail.ru>
2023-04-12 22:05:02 +0300

    NEW

--- main.py
+++ main.py
@@ -4296,21 +4296,89 @@ import csv
 #
 # if __name__ == '__main__':
 #     main()
+# import socket
+# from wiew import index, blog
+#
+# URLS = {
+#     '/': index,
+#     '/blog': blog
+# }
+#
+#
+# def parse_request(request):
+#     parsed = request.split()
+#     method = parsed[0]
+#     url = parsed[1]
+#     return method, url
+#
+#
+# def generate_headers(method, url):
+#     if method != 'GET':
+#         return 'HTTP/1.1 405 Method Not Allowed!\n\n', 405
+#     if url not in URLS:
+#         return 'HTTP/1.1 404 Page Not Found!\n\n', 404
+#     return 'HTTP/1.1 200 OK!\n\n', 200
+#
+#
+# def generate_content(code, url):
+#     if code == 404:
+#         return '<h1>404</h1><h3>Not Found</h3>'
+#     if code == 405:
+#         return '<h1>405</h1><h3>Method Not Allowed</h3>'
+#     return URLS[url]()
+#
+#
+# def generate_response(request):
+#     method, url = parse_request(request)
+#     headers, code = generate_headers(method, url)
+#     body = generate_content(code, url)
+#     return (headers + body).encode()
+#
+#
+# def run():
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind(('127.0.0.1', 5000))
+#     server_socket.listen()
+#
+#     while True:
+#         client_socket, addr = server_socket.accept()
+#         request = client_socket.recv(1024)
+#
+#         print(f"Клиент: {addr} => \n{request.decode('utf-8')}\n")
+#
+#         response = generate_response(request.decode())
+#         client_socket.sendall(response)
+#         client_socket.close()
+#
+#
+# if __name__ == "__main__":
+#     run()
 
-import socket
+
+import sqlite3
 
 
-def run():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('127.0.0.1', 5000))
-    server_socket.listen()
+# con = sqlite3.connect('profile.db')
+# cur = con.cursor()
+#
+# con.close()
 
-    while True:
-        client_socket, abbr = server_socket.accept()
-        request = client_socket.recv(1024)
+with sqlite3.connect('profile.db') as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    summa REAL,
+    date TEXT
+    )""")
+    cur.execute('DROP TABLE users')
 
-        print(f'Клиент: {abbr} => \n{request}\n')
 
 
-if __name__ == '__main__':
-    run()
+
+
+
+
+
+
+
